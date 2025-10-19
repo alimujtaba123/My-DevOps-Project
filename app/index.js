595637d4af183b23a0ea4ffd5c350998d539bb4a
mujji <alimujtabanaqvi53@gmail.com>
2025-10-19 18:40:40 +0500

    project pushed

---
+++ app/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const os = require('os');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+res.json({
+message: 'Hello from DevOps Project!',
+host: os.hostname(),
+time: new Date().toISOString()
+});
+});
+
+
+app.get('/health', (req, res) => res.send('OK'));
+
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
