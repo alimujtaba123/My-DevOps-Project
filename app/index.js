093e59535e0fe45900c4957959b1d6301ad1c93f
mujji <alimujtabanaqvi53@gmail.com>
2025-10-20 14:37:09 +0500

    updated

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 res.json({
-message: 'Hello from Mujtabas Project!',
+message: 'Hello from Mujji Project!',
 host: os.hostname(),
 time: new Date().toISOString()
 });
